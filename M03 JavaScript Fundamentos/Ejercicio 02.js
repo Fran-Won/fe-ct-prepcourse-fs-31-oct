1c3054f1ced66b10e4b2f185c8ee28449746c184
Franco Gomez <60901005+Fran-Won@users.noreply.github.com>
2023-11-01 20:31:27 -0300

    Update Ejercicio 02.js

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,16 +1,23 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
-
+//*googlee las formas de hacer este ejercicio, si estan mal, me gustaria que me lo comenten, gracias :)
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu código: 
+  function reverseString(string) {
+return string.split("").reverse().join("")
+}
+console.log(asesert(reversestring(11>8))) 
+
+function asesert(a,b) {
+    return a===b;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // Tu código: 
 }
 
 function resta(x, y) {
