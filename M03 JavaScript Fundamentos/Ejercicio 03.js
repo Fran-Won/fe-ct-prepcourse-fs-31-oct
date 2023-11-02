44f2c98241dd77f1946638b290c644067aaa437f
Franco Gomez <60901005+Fran-Won@users.noreply.github.com>
2023-11-01 21:06:39 -0300

    Update Ejercicio 03.js

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,6 +5,8 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   
+   
 }
 
 function tienenMismaLongitud(str1, str2) {
